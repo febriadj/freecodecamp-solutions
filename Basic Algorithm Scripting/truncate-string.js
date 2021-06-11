a8337930a032b09fde28bdca6d0bf54481b64903
febriadj <iamfebriadji@gmail.com>
2021-06-11 18:44:18 +0700

    added truncate a string solution

---
+++ Basic Algorithm Scripting/truncate-string.js
@@ -0,0 +1,7 @@
+function truncateString(str, num) {
+  return num < str.length ? str.slice(0, num) + "..." : str;
+}
+
+console.log(
+  truncateString("A-tisket a-tasket A green and yellow basket", 8)
+);
